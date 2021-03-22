678252b26e3c1a349194a967a314da795c91a72f
xingtianxiang <t_xing@u.pacific.edu>
2021-03-22 11:56:49 +0800

    finish database category product and user

--- src/main/java/mapper/CategoryMapper.java
+++ src/main/java/mapper/CategoryMapper.java
@@ -1,4 +1,23 @@
 package mapper;
 
+import model.Category;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface CategoryMapper {
+    void addCategory(Category category);
+
+    void deleteCategory(@Param("categoryId") Integer categoryId);
+
+    void updateCategory(Category category);
+
+    List<Category> getCategory(Integer parentId);
+
+    List<Category> getSecondCategory(); //获取二级分类
+
+    Integer getTotalPage(@Param("parentId")Integer parentId);
+
+    List<Category> page(@Param("offset")Integer offset,@Param("size") Integer size, @Param("parentId")Integer parentId);
 }
